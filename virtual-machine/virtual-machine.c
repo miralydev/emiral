c4e27041a0e7c7ccf5c96feaa5c2eef4fa47cc24
Ruziev Miraly <miraly.dev@gmail.com>
2026-07-02 19:51:46 +0500

    feat: Add fetch part
    Added fetch part and deps.
    + PC/IR Register's
    + Renamed byte_code to bin_code
    + getcmd
    + fetch
    + recv_code skeleton
    
    Signed-off-by: Ruziev Miraly <miraly.dev@gmail.com>

--- virtual-machine/virtual-machine.c
+++ virtual-machine/virtual-machine.c
@@ -4,11 +4,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-uint8_t *byte_code = NULL;
+static uint8_t *bin_code = NULL;
+
+static uint32_t PC = 0;
+static uint32_t *IR = NULL;
+
+static int recv_code(const int argc, const char **argv)
+{
+	/* TODO: Read code from files */
+
+	(void)argv;
+
+	if (argc < 2) {
+		fprintf(stderr, "No input files\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+static uint32_t *getcmd(void)
+{
+	/* Get cmd from bin_code by PC */
+	return NULL;
+}
 
 static int fetch(void)
 {
-	/* Fetch next cmd */
+	IR = getcmd();
+	++PC;
 }
 
 static int decode(void)
@@ -23,6 +47,9 @@ static int execute(void)
 
 int vm_run(const int argc, const char **argv)
 {
+	if (recv_code(argc, argv) < 0)
+		return -1;
+
 	while (fetch()) {
 		decode();
 		execute();
